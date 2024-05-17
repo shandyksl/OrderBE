6bd50972a76978877d26db21db99cf1e35fbe386
LAPTOP-ULR5FPFS\Henry <shandyksl@hotmail.com>
2024-05-17 14:28:10 +0800

    refine code

---
+++ src/main/java/com/example/OrderBE/Initializer/OrderServiceInitializer.java
@@ -0,0 +1,56 @@
+package com.example.OrderBE.Initializer;
+
+import com.example.OrderBE.services.OrderService;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.apache.rocketmq.client.consumer.DefaultLitePullConsumer;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+
+@Component
+public class OrderServiceInitializer {
+
+    private static final Logger logger = Logger.getLogger(OrderServiceInitializer.class.getName());
+
+    private final OrderService orderService;
+    private DefaultLitePullConsumer litePullConsumer;
+    private ScheduledExecutorService executorService;
+
+    @Autowired
+    public OrderServiceInitializer(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @PostConstruct
+    public void init() throws MQClientException {
+        litePullConsumer = new DefaultLitePullConsumer("order-consumer-group-wacao");
+        litePullConsumer.subscribe("placeorder", "*");
+        litePullConsumer.setPullBatchSize(20);
+        litePullConsumer.start();
+        logger.info("Consumer started and subscribed to topic 'placeorder'.");
+
+        executorService = Executors.newSingleThreadScheduledExecutor();
+        executorService.scheduleAtFixedRate(orderService::pollMessages, 0, 1, TimeUnit.SECONDS);
+
+        orderService.setLitePullConsumer(litePullConsumer);
+    }
+
+    @PreDestroy
+    public void shutdown() {
+        if (litePullConsumer != null) {
+            litePullConsumer.shutdown();
+            logger.info("Consumer shut down.");
+        }
+        if (executorService != null) {
+            executorService.shutdown();
+            logger.info("Executor service shut down.");
+        }
+    }
+
+}
--- src/main/java/com/example/OrderBE/services/OrderService.java
+++ src/main/java/com/example/OrderBE/services/OrderService.java
@@ -4,15 +4,18 @@ import com.example.OrderBE.repositories.SalesOrderRepostory;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import lombok.Setter;
 import org.apache.rocketmq.client.consumer.DefaultLitePullConsumer;
 import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.common.message.MessageExt;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import com.fasterxml.jackson.core.type.TypeReference;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -28,35 +31,23 @@ public class OrderService   {
 
     private final ObjectMapper objectMapper = new ObjectMapper();
 
-
     public static volatile boolean running = true;
 
-
     private final SalesOrderRepostory salesOrderRepository; // A
+
+    @Setter
     private DefaultLitePullConsumer litePullConsumer;
-    private ScheduledExecutorService executorService;
 
-
-    public OrderService(SalesOrderRepostory salesOrderRepository){
+    @Autowired
+    public OrderService(SalesOrderRepostory salesOrderRepository ){
         this.salesOrderRepository = salesOrderRepository;
+
     }
 
-
-    @PostConstruct
-    public void init() throws MQClientException {
-        litePullConsumer = new DefaultLitePullConsumer("order-consumer-group-wacao");
-        litePullConsumer.subscribe("placeorder", "*");
-        litePullConsumer.setPullBatchSize(20);
-        litePullConsumer.start();
-        logger.info("Consumer started and subscribed to topic 'placeorder'.");
-
-
-        executorService = Executors.newSingleThreadScheduledExecutor();
-        executorService.scheduleAtFixedRate(this::pollMessages, 0, 1, TimeUnit.SECONDS);
-    }
 
     @Transactional
     public void pollMessages() {
+
         try {
             if (running) {
                 logger.info("Polling for messages...");
@@ -81,30 +72,16 @@ public class OrderService   {
     }
 
     private List<SalesOrder> convertMessageToSalesOrder(MessageExt messageExt) throws IOException {
-
+        List<SalesOrder> salesOrders = new ArrayList<>();
         // Extract the message body as a byte array
         byte[] body = messageExt.getBody();
 
         String bodyString = new String(body, StandardCharsets.UTF_8);
-
-        List<SalesOrder> salesOrders = objectMapper.readValue(bodyString, new TypeReference<List<SalesOrder>>() {});
+        if(!bodyString.isEmpty()){
+          salesOrders = objectMapper.readValue(bodyString, new TypeReference<List<SalesOrder>>() {});
+        }
         return salesOrders;
     }
 
-
-    @PreDestroy
-    public void shutdown() {
-        running = false;
-        if (litePullConsumer != null) {
-            litePullConsumer.shutdown();
-            logger.info("Consumer shut down.");
-
-        }
-        if (executorService != null) {
-            executorService.shutdown();
-            logger.info("Executor service shut down.");
-
-        }
-    }
 
 }
